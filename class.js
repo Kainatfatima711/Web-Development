72efce82271c9b79dcdaa7493292d1c0f6f0ab0e
Kainatfatima711 <kainatfetima@gmail.com>
2025-04-20 17:24:34 +0800

    Add files via upload

--- class.js
+++ class.js
@@ -1,8 +1,6 @@
-var value=Math.acos(-1)
-document.write("Demo 1 test value:" + value + "<br>" )
-
-var value=Math.asin(1)
-document.write("Demo2 test value:" + value +"<br>" )
-
-var value=Math.atan(.5)
-document.write("Demo3 test value:" + value + "<br>" )
+function findMax(){
+    var max=0;
+    for()
+     
+}
+    
