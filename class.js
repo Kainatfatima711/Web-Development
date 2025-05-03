1bf890f3aab573dfc5eaa149b36c86bceec20dd2
Kainatfatima711 <kainatfetima@gmail.com>
2025-05-03 17:17:06 +0800

    Add files via upload

--- class.js
+++ class.js
@@ -1,26 +1,10 @@
-function validate( e ) {
-    e.preventDefault( );
-    
+var text="";
+var i =0;
 
+while  ( i < 10 ){
 
-    const email=document.getElementById("email").value ;
-    const pass=document.getElementById("password").value ;
-    const msgbox =document.getElementById("message");
-
-    let message='';
-    if(email=== ''){
-        message="Please Enter an Email.";
-        msgbox .style.color="red";
-    }else if (pass=== ''){
-        message="Please Enter your Password .";
-        msgbox.style.color="red";
-
-    } 
-
-    else{
-        message="Login Successful !";
-        msgbox.style.color="green";
-    }
-    msgbox.innerText=message;
-    
+    text = text  +" <br> The number is " + i;
+    i++;
 }
+
+document.getElementById("number").innerHTML= text;
