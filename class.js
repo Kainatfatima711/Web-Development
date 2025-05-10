37bb9fb5704a07d82b55940f38d730b12590f8b1
Kainatfatima711 <kainatfetima@gmail.com>
2025-05-10 16:55:32 +0800

    Add files via upload

--- class.js
+++ class.js
@@ -1,31 +1,15 @@
-class student {
-    constructor(name,year) {
-        
-        this.name=name;
-        this.year=year;
+var points = [ 40 ,100 ,1 ,5 ,25 ,10 ];
 
-    }
+document.getElementById("result").innerHTML=points;
 
-    //creating age method for class student
-
-    age(){
-        let date=new Date();
-        return date.getFullYear() - this.year; //2025 - 2020 = 11
-
-    }
+function ascending()  {
+    points.sort(function (a,b) {return a-b} )
+    document.getElementById("result").innerHTML=points;
 }
 
 
-      //use student class with two objects
+function descending() {
+    points.sort(function (a,b) {return b-a})
+    document.getElementById("result").innerHTML=points;
+}
 
-      student1=new student ("Riya",2014 );
-
-      document.getElementById(student1).innerHTML=
-      "My name is" + student1.name + ".I born in" + student1.year + ".And my age is" + student1.age ();
-
-      student2=new student ("Tina", 2018 );
-
-      document.getElementById(student1).innerHTML=
-      "My name is" + student2.name + ".I born in" + student2.year + ".And my age is" + student2.age();
-
-      
